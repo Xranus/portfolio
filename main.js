e4d1514aa95ff589af91629254fdfbf0e010ba6d
Muhammad Hassan <mehassan3454@gmail.com>
2024-10-26 23:17:49 +0500

    home-page

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-
-
-function handleNav() {
-    
-}
-
+const movingdiv = document.getElementById('MD')
+const home = document.getElementById('home')
+window.addEventListener('pointermove', (e)=>{
+    movingdiv.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+home.onmousemove = (e) => {
+    const rect = home.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const rotateX = ((y / rect.height) - 1) * 30;
+    const rotateY = ((x / rect.width) - 1) * -30;
+    home.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+};
+home.onmouseleave = () => {
+    home.style.transform = 'rotateY(0deg) rotateX(0deg)';
+};
