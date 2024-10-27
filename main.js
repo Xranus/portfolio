48d88112df6c05389229f6fa8f42d4fa3e7d1667
Muhammad Hassan <mehassan3454@gmail.com>
2024-10-27 16:48:53 +0500

    ...

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const movingdiv = document.getElementById('MD')
+const Link = document.querySelectorAll('nav span')
+const transition = document.getElementById('skills-transition')
+const transitionHeading = document.querySelector('#skills-transition h1')
 const home = document.getElementById('home')
 window.addEventListener('pointermove', (e)=>{
     movingdiv.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
-home.onmousemove = (e) => {
-    const rect = home.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const rotateX = ((y / rect.height) - 1) * 30;
-    const rotateY = ((x / rect.width) - 1) * -30;
-    home.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
-};
-home.onmouseleave = () => {
-    home.style.transform = 'rotateY(0deg) rotateX(0deg)';
-};
+Link.forEach((el)=>{
+    el.addEventListener('click',async()=>{
+        transitionHeading.textContent = el.textContent
+        transition.classList.toggle('t-active')  
+    })
+})
